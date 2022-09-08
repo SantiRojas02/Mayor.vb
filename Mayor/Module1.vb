a332076b0f949b836d44f40eac1d0fffa03ca983
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-07 22:09:38 -0300

    Mayor.vb

---
+++ Mayor/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main(args As String)
+
+        Dim a, b, c, resultado As Integer
+
+
+
+
+        Console.Write("Ingrese numero1 : ")
+        a = Console.ReadLine()
+        Console.Write("Ingrese numero2 : ")
+        b = Console.ReadLine()
+        Console.Write("Ingrese numero2 : ")
+        c = Console.ReadLine()
+
+        If (a > b) & (a > c) Then
+            resultado = a
+
+        End If
+
+        Console.WriteLine("Numero mayor: " & resultado)
+
+        Console.ReadKey()
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
